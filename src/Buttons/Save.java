be51d6228d2a5cefb1d8ab7a6af2f90cb30faa30
ZenouReimu <blackhole@live.de>
2016-01-16 21:30:38 +0100

    1/13 @ 21:30 Save.java NewGameButton

--- src/Buttons/Save.java
+++ src/Buttons/Save.java
@@ -4,18 +4,17 @@ package Buttons;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.File;
 
 public class Save {
     private static  JFileChooser FileChooser = new JFileChooser();
-    private static FileFilter imageFilter = new FileNameExtensionFilter(".CHSave");
+    private static FileFilter fileFilter = new FileNameExtensionFilter(".CHSave");
     private final String Path = "ChessGameSave";
 
     public static void main(String[] args){
        Save();
     }
     public static void Save(){
-    FileChooser.addChoosableFileFilter(imageFilter);
+    FileChooser.addChoosableFileFilter(fileFilter);
         int choice = FileChooser.showSaveDialog(null);
 
     }
