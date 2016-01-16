c918f36e8c1f97b15e4c876a1e826f0ff06949c1
NBCodeplanner <codeplanner@t-online.de>
2016-01-16 15:44:59 +0100

    Chess Figures
    These are the first basic figure objects, nothing special

---
+++ src/otherObjects/Location.java
@@ -0,0 +1,14 @@
+package otherObjects;
+
+/**
+ * Created by niklasbalazs on 16/01/16.
+ */
+public class Location {
+    int x,y;
+    public Location(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    
+}
