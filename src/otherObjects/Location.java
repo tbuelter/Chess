4890214f64950687059bd9d9ec4b98c5a258d1ce
ZenouReimu <blackhole@live.de>
2016-01-16 19:39:08 +0100

    Location Test

--- src/otherObjects/Location.java
+++ src/otherObjects/Location.java
@@ -4,7 +4,7 @@ package otherObjects;
  * Created by niklasbalazs on 16/01/16.
  */
 public class Location {
-    int x,y;
+    public int x,y;
     public Location(int x, int y){
         this.x = x;
         this.y = y;
